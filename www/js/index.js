78861ec287d52348ccdacb9b71ec1f63f7014134
Daniel Glos <you@example.com>
2018-01-21 18:53:50 +0200

    Done using stock camera

--- www/js/index.js
+++ www/js/index.js
@@ -1,7 +1,47 @@
+function setOptions(srcType) {
+    var options = {
+        // Some common settings are 20, 50, and 100
+        quality: 50,
+        destinationType: Camera.DestinationType.FILE_URI,
+        // In this app, dynamically set the picture source, Camera or photo gallery
+        sourceType: srcType,
+        encodingType: Camera.EncodingType.JPEG,
+        mediaType: Camera.MediaType.PICTURE,
+        allowEdit: false,
+        correctOrientation: true  //Corrects Android orientation quirks
+    }
+    return options;
+}
+
+function openCamera(selection) {
+
+    var srcType = Camera.PictureSourceType.CAMERA;
+    var options = setOptions(srcType);
+
+    navigator.camera.getPicture(function cameraSuccess(imageUri) {
+
+        displayImage(imageUri);
+        // You may choose to copy the picture, save it somewhere, or upload.
+
+    }, function cameraError(error) {
+        console.debug("Unable to obtain picture: " + error, "app");
+
+    }, options);
+}
+
+function displayImage(imgUri) {
+
+    var elem = document.getElementById('img_foto');
+    elem.width = 300;
+    elem.height = 500;
+    elem.src = imgUri;
+}
+
 var app = {
     // Application Constructor
     initialize: function() {
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+        document.getElementById('btn_takePicture').addEventListener('click', openCamera, false);
     },
 
     // deviceready Event Handler
